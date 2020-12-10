9758b863426bb46fb80b10eb3136ec12cd6f49b2
mengxiangjian <mengxiangjian13@163.com>
2020-12-10 17:47:27 +0800

    first commit

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// create new string
+var s = "some new string"
+
+// create new int
+var population = 1_000_000
+
+/// multi lines string
+var ms = """
+this is
+a multi line
+string
+"""
+
+/// 非multi lines，只是为了一行清晰
+var nms = """
+this is not \
+a muti line \
+string
+"""
+
+// double
+let d = 1.0
+// integer
+let i = 1
+// invalid integer can't plus double
+// let sum = d + i
+
+// string interpolation
+let score = 85
+let scoreString = "the score was \(score)"
+
+
